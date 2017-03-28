48c70d1c1031d1a4c7ea9f2df0ff169907dceb47
Luxgnite <ewan.dubois@live.fr>
2017-03-28 03:36:35 +0200

    Début préview image
    * Bug à la fonction ReadAsDataURL() de FileReader qui renvoit un undefined, alors que le File de l'input est bien défini...

--- app/app.js
+++ app/app.js
@@ -13,27 +13,35 @@ cinema.directive("datePicker", function () {
                 }
             });
         }
-    }
+    };
 });
 
-.directive("previewImage", function () {
+cinema.directive('previewImage', function() {
     return {
         require: 'ngModel',
-        link: function (scope, element, attrs, ngModel) {
-            $(element).datepicker({dateFormat: "D dd-M-yy",
-                onSelect: function (date) {
+        link: function(scope, element, attr, ctrl) {
+            console.log(element);
+            var listener = function() {
+                var reader = new FileReader();
+                var file = this.files[0];
+
+                reader.addEventListener("load", function(){
+                    console.log(file);
                     scope.$apply(function () {
-                        ngModel.$setViewValue(date);
+                        ctrl.$setViewValue(url);
                     });
-                }
-            });
+                });
+
+                var url = reader.readAsArrayBuffer(file).result;
+            };
+            element.bind('change', listener);
         }
-    }
+    };
 });
 
 cinema.controller("formController", function ($scope) {
 
     var previewPic = function () {
 
-    }
+    };
 });
