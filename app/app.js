0cf6a9cb02d68745b577ac27015849d634778717
Luxgnite <ewan.dubois@live.fr>
2017-03-28 12:04:40 +0200

    Finalisation création film + début recherche

--- app/app.js
+++ app/app.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var map;
+
+function init () {
+    localStorage.setItem("Movie", JSON.stringify([]));
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
 var cinema = angular.module("cinema", []);
 cinema.directive("datePicker", function () {
     return {
@@ -20,19 +30,12 @@ cinema.directive('previewImage', function() {
     return {
         require: 'ngModel',
         link: function(scope, element, attr, ctrl) {
-            console.log(element);
             var listener = function() {
                 var reader = new FileReader();
-                var file = this.files[0];
-
-                reader.addEventListener("load", function(){
-                    console.log(file);
-                    scope.$apply(function () {
-                        ctrl.$setViewValue(url);
-                    });
-                });
-
-                var url = reader.readAsArrayBuffer(file).result;
+                reader.readAsDataURL(this.files[0]);
+                reader.onloadend = function (e){
+                    ctrl.$setViewValue(e.target.result);
+                };
             };
             element.bind('change', listener);
         }
@@ -41,7 +44,41 @@ cinema.directive('previewImage', function() {
 
 cinema.controller("formController", function ($scope) {
 
-    var previewPic = function () {
+    $scope.submit = function () {
+        var movie = {
+            title: $scope.title,
+            realisator: $scope.realisator,
+            duration: $scope.duration,
+            genre: $scope.genre,
+            date: $scope.date,
+            file: $scope.file
+        };
+        var listMovies = JSON.parse(localStorage.getItem("Movie"));
+        listMovies.push(movie);
+        localStorage.setItem("Movie", JSON.stringify(listMovies));
+    };
+});
 
+cinema.controller("searchController", function($scope) {
+    $scope.keyword = "";
+    $scope.search = function(){
+        var movies = JSON.parse(localStorage.getItem("Movie"));
+        var result = [];
+        var html = "";
+        for (let movie of movies)
+        {
+            if(movie.title === $scope.keyword || movie.realisator === $scope.keyword)
+            {
+                result.push(movie);
+                html = "<img src='"+movie.file+"'/>";
+            }
+
+        }
+        console.log(html);
+        var searchResult = $("#search-result");
+        searchResult.removeClass("hidden");
+        searchResult.addClass("shown");
+
+        searchResult.append(html);
     };
 });
