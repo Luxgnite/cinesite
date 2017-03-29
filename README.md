72b89be84e2c24f29ee16403089df38bb28f803b
Luxgnite <ewan.dubois@live.fr>
2017-03-29 04:32:56 +0200

    Single-page application & fiche film
    * Single-Page Application (ngRoute / ngView)
    * Vue fiche de film
    * Redirection automatique sur fiche du film après création de celui-ci
    * Champ de recherche élargie aux genres

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-cinesite
+# Site de Cinéma
+
+## Fonctionnalités
+
+* Ajouter un film à la base de données et redirection vers sa fiche automatique
+* Rechercher et proposer à l'utilisateur les films dont le titre ou son réalisateur possède les mots-clés entrés
+* Consulter la fiche d'un film
+* Single-Page Application
+
+## Technologies utilisées
+
+* AngularJS (Single-Page Application - ngRoute/ngView, architecture MVC)
+* JQuery & JQuery UI (datepicker, ciblage facilité des éléments)
+* Bootstrap (mise en forme)
--- app/app.js
+++ app/app.js
@@ -5,8 +5,8 @@ var map;
 $(document).ready(function () {
     var movies = [
         {
-          title: "Ce poisson, la vie et moi",
-            realisator: "Moi",
+            title: "Ce poisson, la vie et moi",
+            realisator: "Toi",
             duration: "59:59",
             genre: "Mystère, Horreur, Romance",
             synopsis: "Glou glou. Argh, je suis mort.",
@@ -15,29 +15,57 @@ $(document).ready(function () {
         },
 
         {
-            title: "Ce poisson, la vie et moi",
-            realisator: "Moi",
-            duration: "59:59",
-            genre: "Mystère, Horreur, Romance",
-            synopsis: "Glou glou. Argh, je suis mort.",
+            title: "JSP",
+            realisator: "Anne Onyme",
+            duration: "42:42",
+            genre: "Film d'hauteur",
+            synopsis: "Hmmmm... Le cerveau chauffe",
             date: "11-12-2013",
             file: "https://francaisdefrance.files.wordpress.com/2012/01/grand-moment-de-loose-pour-mathieu-madenian-dans-mot-de-passe_flash_video_background.jpg"
+        },
+        {
+            title: "Ghost in the Shell",
+            realisator: "Rupert Sander",
+            duration: "1h47",
+            genre: "Science Fiction",
+            file: "http://fr.web.img5.acsta.net/c_215_290/pictures/17/03/01/17/45/314818.jpg",
+            synopsis: "Dans un futur proche, le Major est unique en son genre: humaine sauvée d’un terrible accident, son corps aux capacités cybernétiques lui permet de lutter contre les plus dangereux criminels. Face à une menace d’un nouveau genre qui permet de pirater et de contrôler les esprits, le Major est la seule à pouvoir la combattre. Alors qu’elle s’apprête à affronter ce nouvel ennemi, elle découvre qu’on lui a menti : sa vie n’a pas été sauvée, on la lui a volée. Rien ne l’arrêtera pour comprendre son passé, trouver les responsables et les empêcher de recommencer avec d’autres. "
         }
-    ];
+]
+;
 
-    localStorage.setItem("Movie", JSON.stringify(movies));
+localStorage.setItem("Movie", JSON.stringify(movies));
+if (document.getElementById('map') !== null) {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
     });
-});
+}
+})
+;
 
-var cinema = angular.module("cinema", []);
+var cinema = angular.module("cinema", ['ngRoute']);
+
+cinema.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider
+            .when('/movie/:mv', {
+                templateUrl: 'movie.html',
+                controller: 'movieCtrl'
+            })
+            .when('/addMovie', {
+                templateUrl: 'addMovie.html',
+                controller: 'addMovieCtrl'
+            });
+    }
+]);
+
 cinema.directive("datePicker", function () {
     return {
         require: 'ngModel',
         link: function (scope, element, attrs, ngModel) {
-            $(element).datepicker({dateFormat: "D dd-M-yy",
+            $(element).datepicker({
+                dateFormat: "D dd-M-yy",
                 onSelect: function (date) {
                     scope.$apply(function () {
                         ngModel.$setViewValue(date);
@@ -48,14 +76,15 @@ cinema.directive("datePicker", function () {
     };
 });
 
-cinema.directive('previewImage', function() {
+
+cinema.directive('previewImage', function () {
     return {
         require: 'ngModel',
-        link: function(scope, element, attr, ctrl) {
-            var listener = function() {
+        link: function (scope, element, attr, ctrl) {
+            var listener = function () {
                 var reader = new FileReader();
                 reader.readAsDataURL(this.files[0]);
-                reader.onloadend = function (e){
+                reader.onloadend = function (e) {
                     ctrl.$setViewValue(e.target.result);
                 };
             };
@@ -64,34 +93,36 @@ cinema.directive('previewImage', function() {
     };
 });
 
-cinema.controller("formController", function ($scope) {
+cinema.controller("addMovieCtrl", function ($scope) {
 
     $scope.submit = function () {
+        console.log('lol');
         var movie = {
             title: $scope.title,
             realisator: $scope.realisator,
             duration: $scope.duration,
             genre: $scope.genre,
             date: $scope.date,
-            file: $scope.file
+            file: $scope.file,
+            synopsis: $scope.synopsis
         };
         var listMovies = JSON.parse(localStorage.getItem("Movie"));
         listMovies.push(movie);
         localStorage.setItem("Movie", JSON.stringify(listMovies));
+        window.location.href = "#movie/" + (listMovies.length - 1);
     };
 });
 
-cinema.controller("searchController", function($scope) {
+cinema.controller("searchController", function ($scope) {
     $scope.keyword = "";
-    $scope.search = function(){
+    $scope.search = function () {
         var searchResult = $("#search-result");
 
-        if($scope.keyword !== "") {
+        if ($scope.keyword !== "") {
             searchResult.addClass("shown");
             searchResult.removeClass("hidden");
         }
-        else
-        {
+        else {
             searchResult.addClass("hidden");
             searchResult.removeClass("shown");
         }
@@ -102,17 +133,22 @@ cinema.controller("searchController", function($scope) {
         var result = [];
         var html = "";
         searchResult.empty();
-        for (let movie of movies)
-        {
-            if(movie.title.includes($scope.keyword) && $scope.keyword!=="" || movie.realisator.includes($scope.keyword) && $scope.keyword!=="")
-            {
-                console.log(movie);
-                result.push(movie);
-                searchResult.append("<figure class='result'><img src='"+movie.file+"'/>" +
-                    "<figcaption>"+movie.title+"</figcaption></figure>");
+        for (var i = 0; i < movies.length; i++) {
+            if (movies[i].genre.includes($scope.keyword) && $scope.keyword !== ""
+                ||movies[i].title.includes($scope.keyword) && $scope.keyword !== ""
+                || movies[i].realisator.includes($scope.keyword) && $scope.keyword !== "") {
+                console.log(movies[i]);
+                result.push(movies[i]);
+                searchResult.append("<figure class='result'><a href='#movie/" + i + "'><img src='" + movies[i].file + "'/></a>" +
+                    "<figcaption>" + movies[i].title + "</figcaption></figure>");
             }
 
         }
 
     };
 });
+
+cinema.controller("movieCtrl", ['$scope', '$routeParams',
+    function ($scope, $routeParams) {
+        $scope.movie = JSON.parse(localStorage.getItem("Movie"))[$routeParams.mv];
+    }]);
